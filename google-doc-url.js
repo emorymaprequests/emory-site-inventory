76d928a5c95ae5d317cd79890ae1f81eec69d35e
emorymaprequests <86745571+emorymaprequests@users.noreply.github.com>
2021-06-30 16:32:48 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JkdZZ1BBT4huiwKnz1a159XvyH3Se-2btMn6UlM3IZc/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_2gjYUrfe-zM7pKl5AJn5UrJ4ZkLCZ4thMkgODyJsOI/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCPRGKxsjReTCX2x8jimLwpUU5oUGzQQMU';
